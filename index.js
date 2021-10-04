7ad3f5f41a5382c43c113bbb2eddffbf1c8178aa
unknown <amery.bur410@gmail.com>
2021-10-04 18:22:02 -0500

    adding initial file structure.

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const engineer = require("engineer");
+const intern = require("intern");
+const manager = require("manager");
+const 
